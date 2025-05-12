07d2203df07fc56f1392496f6b73133f26477e51
Alexander-Oni <alex.evant1448@gmail.com>
2025-05-12 18:38:18 +0300

    Finished first Lab

--- Lab_1/lab_1.py
+++ Lab_1/lab_1.py
@@ -1,11 +1,17 @@
 from math import *
 
+def calculate_z1(a):
+    return (sin(a) ** 2 - tan(a) ** 2) / (cos(a) ** 2 - (1 / tan(a)) ** 2)
+
+def calculate_z2(a):
+    return tan(a) ** 6
+
 while True:
     angle = float(input("Enter the value of the angle in degrees: "))
     a = radians(angle)
     if angle % 90 != 0:
-        z1 = (sin(a) ** 2 - tan(a) ** 2)/(cos(a) ** 2 - (1/tan(a)) ** 2)
-        z2 = tan(a) ** 6
+        z1 = calculate_z1(a)
+        z2 = calculate_z2(a)
         print("Angle: {0:.2f}\u00b0 Z1: {1:.5f}".format(angle, z1))
         print("Angle: {0:.2f}\u00b0 Z2: {1:.5f}".format(angle, z2))
     else:
